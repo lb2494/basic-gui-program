1de28757970c7ed2494d608db4337e6a6b24995d
Lakshay <lb2494@gmail.com>
2018-07-14 15:22:05 +0530

    debugged

--- dbbook.py
+++ dbbook.py
@@ -1,18 +1,20 @@
 import sqlite3
-db=sqlite.connect(listofbooks.db)
+db=sqlite3.connect("listofbooks.db")
 cur=db.cursor()
 #this will create a table 
 cur.execute('''create table books (
 BookId integer primary key autoincrement,
 Title text(20),
 Author text(30),
-Price real;''')
+Price real);''')
 #this will add records into the table
 try:
-cur.execute('''Insert into book values(3,'Think Python','Allen B. Drowney',475);''')
-cur.execute('''Insert into book values(4,'C Programming','Dennis and Richie',300);''')
-cur.execute('''Insert into book values(5,'Guide to network Programming','Beej',550);''')
-cur.execute('''Insert into book values(1,'Git magic','Ben Lynn',500);''')
-db.close()
-db.commit()
+    cur.execute('''Insert into books values(3,'Think Python','Allen B. Drowney',475);''')
+    cur.execute('''Insert into books values(4,'C Programming','Dennis and Richie',300);''')
+    cur.execute('''Insert into books values(5,'Guide to network Programming','Beej',550);''')
+    cur.execute('''Insert into books values(1,'Git magic','Ben Lynn',500);''')
+    db.commit()
+except:
+    db.rollback()
+    db.close()
 
